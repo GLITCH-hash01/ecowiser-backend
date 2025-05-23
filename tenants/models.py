41e9f301a50a03b151fec16e125eda77165c10af
Glitch-hash01 <harikichus2018@gmail.com>
2025-05-23 22:30:08 +0530

    First commit

---
+++ tenants/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Tenant(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name

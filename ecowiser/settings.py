e07d33000870329611cb37acca464fbf22d83f0d
Glitch-hash01 <harikichus2018@gmail.com>
2025-05-29 20:33:30 +0530

    feat: Update CORS_ALLOWED_ORIGINS to include frontend URL

--- ecowiser/settings.py
+++ ecowiser/settings.py
@@ -53,7 +53,8 @@ INSTALLED_APPS = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',  
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://ecowiser-frontend.vercel.app'
 ]
 
 CORS_ALLOW_CREDENTIALS = True
